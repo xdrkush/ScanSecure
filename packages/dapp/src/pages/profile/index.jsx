db31f103d2630c5ffb6d46fdef94da006f1e8d83
xdrkush <drkushikush@gmail.com>
2023-07-23 16:20:43 +0200

    clean: script -> seed & centralised abi

--- packages/dapp/src/pages/profile/index.jsx
+++ packages/dapp/src/pages/profile/index.jsx
@@ -2,14 +2,19 @@ import { Box, Grid, Heading } from "@chakra-ui/react"
 import ProfileLayout from "../../components/layouts/Profile.layout"
 import { AskCertification } from "../../components/profile/AskCertification"
 import { BuyTicket } from "../../components/ticket/BuyTicket"
+import { ScanSecureContext } from "../../contexts"
+import { useContext } from "react"
 
 export default function Profile() {
+    const { isWhitelisted } = useContext(ScanSecureContext)
     return (
         <ProfileLayout>
-            <Heading size="md">My Ticket</Heading>
-            <Grid py={3} minH={"20vh"}>
-                <AskCertification />
-            </Grid>
+            <Heading size="md">My Profile</Heading>
+            {!isWhitelisted && (
+                <Grid py={3} minH={"20vh"}>
+                    <AskCertification />
+                </Grid>
+            )}
             <Grid py={3} minH={"20vh"}>
                 <BuyTicket />
             </Grid>
